1dc95a292c2ebcab3662c903e7b190d9e60d9843
Abdiel Athayde <abdieldeathayde67@gmail.com>
2023-09-14 22:29:53 -0300

    atualizando projeto programação concorrente

---
+++ src/clientAndServer/Client.java
@@ -0,0 +1,66 @@
+package clientAndServer;
+import java.net.*;
+import java.io.*;
+
+public class Client implements Runnable {
+	public DatagramSocket aSocket = null;
+
+	public Client(DatagramSocket aSocket) {
+		this.aSocket = aSocket;
+	}
+
+	public static void main(String args[]) {
+		// args give message contents and server hostname
+		DatagramSocket aSocket = null;
+		
+		
+			
+		while (true) {
+			try {
+			
+				aSocket = new DatagramSocket();
+				
+				String msg = "ping";
+				byte[] m = msg.getBytes();
+				InetAddress aHost = InetAddress.getByName("localhost");
+				int serverPort = 6789;
+				DatagramPacket request = new DatagramPacket(m, msg.length(),
+						aHost, serverPort);
+				
+				// Send a message
+				Thread.sleep(1000);
+				aSocket.send(request);
+				
+				// Prepare a empty message for a response
+				byte[] buffer = new byte[1000];
+				DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+				
+				//Receive response
+				aSocket.receive(response);
+				
+				// Print response
+				String resp = new String(response.getData());
+				System.out.println("Client received: " + resp);
+			} catch (IOException e) {
+				System.err.println("Erro ao lidar com a conexão do cliente: " + e.getMessage());
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				aSocket.close();
+			}
+		} 
+	}
+			
+	@Override
+	public void run() {
+		try {
+			aSocket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	
+
+	
+}
---
+++ src/clientAndServer/Server.java
@@ -0,0 +1,46 @@
+package clientAndServer;
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	public static void main(String args[]) {
+		DatagramSocket aSocket = null;
+			
+		while (true) {
+			try {
+				// Create a socket on port 6789
+				aSocket = new DatagramSocket(6789);
+				
+				
+				Thread clientThread = new Thread(new Client(aSocket));
+				clientThread.start();
+				// Prepare an empty message
+				byte[] buffer = new byte[1000];
+				DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+				
+				// Receive a message
+				aSocket.receive(request);
+				
+				
+				// Prepare a response
+				String resp = "pong " + new String(buffer);
+				byte[] m = resp.getBytes();
+				// Obtain response address/port from request
+				DatagramPacket response = new DatagramPacket(m, resp.length(),
+						request.getAddress(), request.getPort());
+				
+				// Send response
+				System.out.println("Msg: " + resp);
+				Thread.sleep(1000);
+				aSocket.send(response);
+			} catch (java.net.BindException e) {
+				System.err.println("A porta " + aSocket + " Já está em uso");
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			finally {
+				aSocket.close();
+			}
+		} 
+	}
+}
